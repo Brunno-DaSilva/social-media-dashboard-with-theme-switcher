1e4d883772c4ec8fb11e7e9823b3f20ecbd205fd
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-06-24 19:02:56 -0500

    Clear up the js code

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-console.log("Ciao Bella");
-
 let lightMode = localStorage.getItem("lightMode");
 
 const lightModeToggle = document.querySelector("#light-mode-toggle");
 
 const enablelightMode = () => {
-  // 1. Add the class to the body
   document.body.classList.add("lightMode");
-  // 2. Update lightMode in localStorage
+
   localStorage.setItem("lightMode", "enabled");
 };
 
 const disablelightMode = () => {
-  // 1. Remove the class from the body
   document.body.classList.remove("lightMode");
-  // 2. Update lightMode in localStorage
+
   localStorage.setItem("lightMode", null);
 };
 
@@ -23,13 +19,10 @@ if (lightMode === "enabled") {
 }
 
 lightModeToggle.addEventListener("click", () => {
-  // get their lightMode setting
   lightMode = localStorage.getItem("lightMode");
 
-  // if it not current enabled, enable it
   if (lightMode !== "enabled") {
     enablelightMode();
-    // if it has been enabled, turn it off
   } else {
     disablelightMode();
   }
